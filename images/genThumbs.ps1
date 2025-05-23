e04054129a6118f563c7052820c8075a9df6da0c
SR <115826702+RGJP@users.noreply.github.com>
2025-05-22 22:45:22 -0400

    x
    x

---
+++ images/genThumbs.ps1
@@ -0,0 +1,10 @@
+# Requires ImageMagick (magick) to be in PATH
+Get-ChildItem -Filter *.webp | ForEach-Object {
+    $inputFile = $_.FullName
+    $baseName = $_.BaseName
+    $outputFile = "$($_.DirectoryName)\$baseName-thumb.webp"
+
+    magick "$inputFile" -resize 1000x1000^> -quality 25 -define webp:method=6 -define webp:low-memory=true "$outputFile"
+}
+
+pause
