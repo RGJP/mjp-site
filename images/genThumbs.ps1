70aa34d709eaa977ce53dd25e447d8de3207a931
SR <115826702+RGJP@users.noreply.github.com>
2025-05-22 22:52:48 -0400

    x
    x

--- images/genThumbs.ps1
+++ images/genThumbs.ps1
@@ -4,7 +4,7 @@ Get-ChildItem -Filter *.webp | ForEach-Object {
     $baseName = $_.BaseName
     $outputFile = "$($_.DirectoryName)\$baseName-thumb.webp"
 
-    magick "$inputFile" -resize 1000x1000^> -quality 25 -define webp:method=6 -define webp:low-memory=true "$outputFile"
+    magick "$inputFile" -resize 500x500^> -quality 25 -define webp:method=6 -define webp:low-memory=true "$outputFile"
 }
 
 pause
