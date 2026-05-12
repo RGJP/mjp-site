9a4fe256b835bb4089a77cc962f0fc1adbd81b83
RGJP <rgjp@example.com>
2026-05-12 10:41:57 -0400

    updated invitation

---
+++ push.cmd
@@ -0,0 +1,8 @@
+cd C:\Users\rapha\Documents\GitHub\mjp-site
+git config user.name "RGJP"
+git config user.email "rgjp@example.com" 
+git status
+git add -A
+git commit -m "updated"
+git push origin main
+pause
